4c97304e9262d00b83c40ecc38ca48c7011e6ea0
ykatakuri <yannatkk@gmail.com>
2022-01-31 21:41:49 +0100

    display data inside face-snap component

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageURL!: string;
 
+  ngOnInit(): void {
+    this.title = "John Doe";
+    this.description = "L'homme le plus recherché du monde."
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageURL = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }
