644bc90bcea2ccc0df4ab096af6b18d99666c36f
ykatakuri <yannatkk@gmail.com>
2022-02-02 22:43:04 +0100

    dynamic style and class added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
         description: 'Mon meilleur ami depuis tout petit !',
         imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 350,
         location: 'Toulouse'
       },
       {
