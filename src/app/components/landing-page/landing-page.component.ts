585ce8573aad7f226df99d5a32da2f929c9795fd
ykatakuri <yannatkk@gmail.com>
2022-02-25 22:45:25 +0100

    navigation between routes

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private faceSnapRouter: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onContinue(): void {
+    this.faceSnapRouter.navigateByUrl("facesnaps");
   }
 
 }
