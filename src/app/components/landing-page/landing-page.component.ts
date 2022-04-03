06f686ea3afe0f50880210afb705efd7faf1491d
yann <yannatkk@gmail.com>
2022-04-03 17:29:52 +0200

    create form for newsletter with template forms

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  userEmail!: string;
 
   constructor(private faceSnapRouter: Router) { }
 
@@ -15,6 +17,10 @@ export class LandingPageComponent implements OnInit {
 
   onContinue(): void {
     this.faceSnapRouter.navigateByUrl("facesnaps");
+  }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
 
 }
