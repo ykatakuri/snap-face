bdeac4eca6f9290a35271216de54f855a049d6f3
yann <yannatkk@gmail.com>
2022-04-03 18:02:19 +0200

    create reactive form to add face snap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
 
 const routes: Routes = [
     { path: "facesnaps/:id", component: SingleFaceSnapComponent },
     { path: "facesnaps", component: FaceSnapListComponent },
+    { path: "addfacesnap", component: NewFaceSnapComponent },
     { path: "", component: LandingPageComponent },
 ];
 
---
+++ src/app/components/new-face-snap/new-face-snap.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewFaceSnapComponent } from './new-face-snap.component';
+
+describe('NewFaceSnapComponent', () => {
+  let component: NewFaceSnapComponent;
+  let fixture: ComponentFixture<NewFaceSnapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewFaceSnapComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewFaceSnapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
