6b76b46a9267077d9b71e5a07a0e36ec7d3a6125
yann <yannatkk@gmail.com>
2022-04-08 19:21:11 +0200

    create feature module landing-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LandingPageComponent } from './landing-page/components/landing-page/landing-page.component';
 import { FaceSnapListComponent } from './face-snaps/components/face-snap-list/face-snap-list.component';
 import { NewFaceSnapComponent } from './face-snaps/components/new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './face-snaps/components/single-face-snap/single-face-snap.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { FaceSnapsModule } from './face-snaps/face-snaps.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
     CoreModule,
-    FaceSnapsModule
+    FaceSnapsModule,
+    LandingPageModule
   ],
   bootstrap: [AppComponent]
 })
