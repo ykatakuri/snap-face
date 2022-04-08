4431e45b33fb204f7ab7fa80cb208bf761336333
yann <yannatkk@gmail.com>
2022-04-08 16:44:44 +0200

    create auth interceptor

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private token = 'MyFakeToken';
+  
+  getToken(): string {
+    return this.token;
+  }
+
+  constructor() { }
+}
